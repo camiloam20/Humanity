0259fc3b0fccfe199bd1bc3b106631959bab0cfb
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2024-10-16 23:44:20 -0500

    Added everything that is related to home screen

---
+++ lib/screens/saved_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GuardadoScreen extends StatelessWidget {
+
+  const GuardadoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Guardado', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
